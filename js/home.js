17240e726efc7eaf2fd24ed33334862c4ceb62d6
luisportillo <lcpdiseno@gmail.com>
2017-06-27 12:32:39 -0500

    Armado completo de buscador

--- js/home.js
+++ js/home.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
+     var filtrosContainer = $("#vuelos .filtrosContainer")
 
+    //Ver Opciones avanzadas en vuelos
+    $("#verOpciones").click(function( e ){
+        e.preventDefault()
+        filtrosContainer.toggle(500)
+    })
+
+    //Activa carrusel de promociones 
     $('#promociones').owlCarousel({
         items: 3,
         loop: true,
@@ -7,4 +15,41 @@ $(document).ready(function(){
         margin: 10
     });
 
+    //Activa y desactiva Tooltip de viajeros
+    $(".inputViajeros").focus(function (){
+        var padre = $(this).parent();
+        var tooltip = padre.children(".tooltipElement")
+        tooltip.slideDown(500)
+    })
+    $(".listo").click(function ( e ){   
+        e.preventDefault()
+        $(this).parent().parent().slideUp(300)
+    })
+
+    $("#radioVueloRedondo").click(function () {
+        $("#formVueloRedondo").slideDown( 300 , function (){
+            $("#formVueloSencillo").slideUp(100)
+            $("#formVueloArma").slideUp(100)
+        })
+        
+        
+    })
+
+    $("#radioVueloSencillo").click(function () {
+        $("#formVueloSencillo").slideDown( 300 , function () {
+            $("#formVueloRedondo").slideUp(100)
+            $("#formVueloArma").slideUp(100)
+        })
+        
+        
+    })
+
+    $("#radioVueloArma").click( function () {
+        $("#formVueloArma").slideDown( 300 , function () {
+            $("#formVueloSencillo").slideUp(100)
+            $("#formVueloRedondo").slideUp(100)
+        })
+        
+        
+    })
 });
